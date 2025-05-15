939f2f7363f031b9fc2cdd69bad848d5c49e0b16
eguono imonieroh <imonieroheguono@gmail.com>
2025-05-15 15:06:49 +0100

    fiexe: w-full className in the mapped data  in AllTasks.jsx page

--- src/pages/AllTasks.jsx
+++ src/pages/AllTasks.jsx
@@ -22,7 +22,7 @@ const AllTasks = () => {
         {taskData.map((task)=>{
           const {_id,description,tag,taskTitle} = task
           return(
-            <div key={_id} className='border-[#B8B6B6] border-[0.5px] rounded-[10px] py-6 px-3 '>
+            <div key={_id} className='border-[#B8B6B6] w-full border-[0.5px] rounded-[10px] py-6 px-3 '>
               <MappedTasks taskTitle={taskTitle} _id={_id} description = {description} tag={tag} />
             </div>
           )
